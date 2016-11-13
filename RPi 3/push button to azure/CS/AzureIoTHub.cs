2e882e4cf8329955fa98a3978016875a11df54a2
JSharp1 <JSharp1@users.noreply.github.com>
2016-11-13 15:15:45 +0000

    Add files via upload

---
+++ RPi 3/push button to azure/CS/AzureIoTHub.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+
+static class AzureIoTHub
+{
+    //
+    // Note: this connection string is specific to the device "buttontoazure". To configure other devices,
+    // see information on iothub-explorer at http://aka.ms/iothubgetstartedVSCS
+    //
+    const string deviceConnectionString = "{your connection string here}";
+    internal static int data;
+
+    public static async Task SendDeviceToCloudMessageAsync()
+    {
+        var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Amqp);
+        var str = data.ToString();
+        var message = new Message(Encoding.ASCII.GetBytes(str));
+        await deviceClient.SendEventAsync(message);
+    }
+
+    public static async Task<string> ReceiveCloudToDeviceMessageAsync()
+    {
+        var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Amqp);
+
+        while (true)
+        {
+            var receivedMessage = await deviceClient.ReceiveAsync();
+
+            if (receivedMessage != null)
+            {
+                var messageData = Encoding.ASCII.GetString(receivedMessage.GetBytes());
+                await deviceClient.CompleteAsync(receivedMessage);
+                return messageData;
+            }
+
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
+    }
+
+    internal static Task SendDeviceToCloudMessageAsync(object data)
+    {
+        throw new NotImplementedException();
+    }
+}
